3cfe069b3b67c97d7ccfd0c1beb82bf50eab5039
Nattarika Jaroenpat <62160151@go.buu.ac.th>
2020-10-18 15:49:29 +0700

    UI register

--- UI.cpp
+++ UI.cpp
@@ -4,5 +4,40 @@
 #include"Staff.h"
 using namespace std;
 class UI{
-	
+	private:
+		string Name;
+		string Lastname;
+		string password;
+		string Type;
+	public:
+		void menu(){
+			do{
+	  cout << " ==================================================="  <<endl;
+	  cout << "            Welcome To Lion Book Store              "  <<endl;
+	  cout << " ==================================================="  <<endl;
+      cout << "                 1. Register                        "  <<endl;
+      cout << "                 2. Login                           "  <<endl;
+      cout << "                 3. Exit                            "  <<endl;
+      cout << " ================================================== "  <<endl;
+      cout << " Select Menu :"                                        <<endl;
+	  cin >> menu;
+	    if(menu==1){
+               cout << " Name : " ;
+               cin >> Name ;
+               cout << " Lastname : ";
+               cin >> Lastname;
+               cout << "Password : ";
+               cin >> password ;
+	             }
+	    else {
+		cout << " Failed Register " <<endl;
+		
+        }
+        if(menu==2){
+        	
+        	
+		}
+     }while(menu!=3)
+}
+		}
 };
