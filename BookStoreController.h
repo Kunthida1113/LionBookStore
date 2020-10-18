372c078421d0f9c7ced326c366cff557a97beb60
Kunthida Saelee <62160324@go.buu.ac.th>
2020-10-18 23:40:19 +0700

    รวม controller

---
+++ BookStoreController.h
@@ -0,0 +1,24 @@
+#ifndef BOOKSTORECONTROLLER_H
+#define BOOKSTORECONTROLLER_H
+#include<iostream>
+#include<string>
+#include<fstream>
+#include"Book.h"
+#include"Member.h"
+#include"DbLinkedList.h"
+
+class BookStoreController{
+	private:
+		DbLinkedList *BookList;
+	public:
+		BookStoreController();
+		~BookStoreController();
+		void LoadInfoBook();
+		void showAll();
+		void SelectCategoryBook();
+		Book SearchBook(string NameBook);
+		Book RentBook(string IdBook);
+		double Calculate(Member m,Book b);
+		bool Pay(string AmountofMoney,string IDCreditCard,string IDBill);
+};
+#endif
