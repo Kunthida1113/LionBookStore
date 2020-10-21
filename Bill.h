5aec69f3fd36820d66ab13cd1ac1b5e2c4f6d8cf
LAPTOP-UL3OMFPR\windows <woon.jidapa43@gmail.com>
2020-10-21 18:53:48 +0700

    create bill

---
+++ Bill.h
@@ -0,0 +1,13 @@
+class Bill{
+	private:
+			string IDBill;
+			double amountOfMoney;
+	public:
+			bill();
+			void setBill();
+			double Calculate();
+			double CalculateRent();
+			double CalculateBuy();
+			double CalculatePre();
+};
+
