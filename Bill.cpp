d11e4b26c20d4a2464eda03c8f01b6d64b3cfa94
Khun-Earth <72852721+Khun-Earth@users.noreply.github.com>
2020-10-21 19:39:20 +0700

    update

--- Bill.cpp
+++ Bill.cpp
@@ -1,46 +1,53 @@
 #include "Bill.h"> 
-	  Bill::Bill(){
-			IDBill = "";
-			amountOfMoney = 0;
-	   }
+	Bill::Bill(){
+		IDBill = "";
+		amountOfMoney = 0;
+	}
 
-	   void Bill::setBill(){
+	void Bill::setBill(){
+		
+	}
+	  
+	double Bill::Calculate(){
+	 	double total;
+	 	
+	 	
+	 	return total;
+	}
 
-	  }
+	double Bill::CalculateRent(DbLinkedList * L){
+	  	int size = 0;
+	  	float total;
+	  	Book b;
+	  	size = L->size();
+	  	for(int i=0;i<size;i++){
+	  		b = L->getBook();
+	  		total += b.getRentPrice();
+		  }
+	return total;
+	}
 	  
-	  Bill::Calculate(){
-	  	
-	  }
-
-	  Bill::CalculateRent(){
-	  	
-	  }
+	double Bill::CalculateBuy(DbLinkedList * L){
+	  	int size = 0;
+	  	float total;
+	  	Book b;
+	  	size = L->size();
+	  	for(int i=0;i<size;i++){
+	  		b = L->getBook();
+	  		total += b.getBuyPrice();
+		  }
+	return total;	
+	}
 	  
-	  Bill::CalculateBuy(){
-	  	
-	  }
+	double Bill::CalculatePre(DbLinkedList * L){
+	  	int size = 0;
+	  	float total;
+	  	Book b;
+	  	size = L->size();
+	  	for(int i=0;i<size;i++){
+	  		b = L->getBook();
+	  		total += b.getPrePrice();
+		  }
+	return total;
+	}
 	  
-	  Bill::CalculatePre(){
-	  	
-	  }
-	  
-//
-//	   void Bill::printBill(){
-//		cout << "----------------------------------------" << endl;
-//		cout << "	    =   HOOK PEEP BOOKSTORE  = 	 " << endl;
-//	//	cout << " 			         AB00000 " << endl;
-//	//	cout << "			   Date 29/08/63 " << endl;
-//	//	cout << "	Tax#101010101010 (VAT Include)   " << endl;
-// 		cout << "   address : 88/999 Bangsean		 " << endl;
-//		cout << "	   Chonburi 10100		 " << endl;
-//	//	cout << "  Time :  		 " << endl;
-//		cout << "========================================" << endl;
-//		cout << "					 " << endl;
-//		cout << "----------------------------------------" << endl;
-//
-//		cout << "========================================" << endl;
-//		cout << "	w & J NETWORK Center : 1292	 " << endl;
-//		cout << "	      = VAT INCLUDE = 	         " << endl;
-//		cout << "          THANK YOU AND WELCOME	 " << endl;
-//		cout << "----------------------------------------" << endl;
-//	   }
