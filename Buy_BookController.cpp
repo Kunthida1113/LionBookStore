f2044c1799d24870ccb8342c0ca3b7a6808d574a
Khun-Earth <72852721+Khun-Earth@users.noreply.github.com>
2020-10-21 19:32:30 +0700

    update

--- Buy_BookController.cpp
+++ Buy_BookController.cpp
@@ -1,21 +1,33 @@
 #include"Buy_BookController.h"
 
 Book Buy_BookController::SelectCategoryBook(){
-	
+
 }
 
 Book Buy_BookController::SerachBook(string NameBook){
 
 }
 
-Book Buy_BookController::BuyBook(string IdBook){
+void Buy_BookController::BuyBook(string IdBook){
+	Book B;
+ 	B = BookList->getBook(IdBook);
+    	buyBookList->AddBook(B);
 	
 }
 
 double Buy_BookController::Calculate(Book b){
-
+	double sum;
+	sum = b.getBuyPrice() * 1;
+	
+	return sum;
 }
 
 bool Buy_BookController::Pay(string AmountofMoney,string IDCreditCard,string IDBill){
+	double money;
+	
+//	stringstream m(AmountofMoney);
+//	m >> money;
+	
+	// Bill ? 
 	
 }
--- Buy_BookController.h
+++ Buy_BookController.h
@@ -3,12 +3,13 @@
 #include<sstream>
 #include"Book.h"
 #include"Member.h"
+#include "BookStoreController.h"
 
 class Buy_BookController{
 	public:
 		Book SelectCategoryBook();
 		Book SerachBook(string NameBook);
-		Book BuyBook(string IdBook);
+		void BuyBook(string IdBook);
 		double Calculate(Book b);
 		bool Pay(string AmountofMoney,string IDCreditCard,string IDBill);
 };
