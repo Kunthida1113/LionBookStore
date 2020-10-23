d046dca48a3e0bb552850361df4e3efe10f6f1b3
Khun-Earth <72852721+Khun-Earth@users.noreply.github.com>
2020-10-23 13:54:00 +0700

    Update BookStoreController.cpp

--- BookStoreController.cpp
+++ BookStoreController.cpp
@@ -19,6 +19,8 @@ void BookStoreController::ReadFile(string nameFile){
         cout<<"Can not open file"<<endl;
     }
 }
+
+
 void BookStoreController::LoadInfoBook(){
     Book B;
     string line,idBook,nameBook,author,year,cate,rentPrice,buyPrice,prePrice;
@@ -88,7 +90,7 @@ void BookStoreController::LoadInfoBook(){
                     }//else check
                 }//if find
             }//for
-            B.SetBook(idBook,nameBook,author,year,cate,tempRent,tempBuy,tempPre);
+            B.SetBook(idBook,nameBook,author,year,cate,tempRent,tempBuy,0);
         //    cout<<B.getId()<<B.getName()<<B.getAuthor()<<B.getYear()<<endl;
             BookList->AddBook(B);
         }//while
@@ -98,6 +100,85 @@ void BookStoreController::LoadInfoBook(){
         cout<<"Can not open file"<<endl;
     }//else open
 }
+
+void BookStoreController::LoadPreBook(){
+    Book B;
+    string line,idBook,nameBook,author,year,cate,rentPrice,buyPrice,prePrice;
+    string tempId;
+    double tempRent,tempBuy,tempPre;
+    int check=0,length=0,j=0;
+    unsigned int find=0;
+    ifstream BookFile("Pre_book.dat",ios::in);
+    if(BookFile.is_open()){
+        while(getline(BookFile,line)){
+            length = line.length();
+            check=0;
+            j++;
+            for(int i=0;i<length;i++){
+                find = line.find(',');
+                if(find != 0){
+                    check++;
+                    if(check==1){
+                        idBook = line.substr(0,find);
+                        line.erase(0,find+1);
+                        tempId = idBook.substr(0,2);
+                        if(tempId=="00"){
+                            cate = "Cartoon";
+                        }else if(tempId=="01"){
+                            cate = "Children and Youth";
+                        }else if(tempId=="02"){
+                            cate = "Education";
+                        }else if(tempId=="03"){
+                            cate = "Foreign";
+                        }else if(tempId=="04"){
+                            cate = "General Knowledge";
+                        }else if(tempId=="05"){
+                            cate = "Health";
+                        }else if(tempId=="06"){
+                            cate = "Infotainment & Art";
+                        }else if(tempId=="07"){
+                            cate = "Literature";
+                        }else if(tempId=="08"){
+                            cate = "Magazine";
+                        }else if(tempId=="09"){
+                            cate = "Techno";
+                        }
+                    }else if(check==2){
+                        nameBook = line.substr(0,find);
+                        line.erase(0,find+1);
+                    }else if(check==3){
+                        author = line.substr(0,find);
+                        line.erase(0,find+1);
+                    }else if(check==4){
+                        year = line.substr(0,find);
+                        line.erase(0,find+1);
+                    }else if(check==5){
+                        rentPrice = line.substr(0,find);
+                        line.erase(0,find-1);
+                        //rentPrice string to double
+                        stringstream tmp(rentPrice);
+                        tmp>>(tempRent);
+                    }else if(check==6){
+                        buyPrice = line.substr(2,length);
+                        line.erase(0,length);
+                         //buyPrice string to double
+                        stringstream tmp2(buyPrice);
+                        tmp2>>(tempBuy);
+                        //calculate prePrice
+                        tempPre = tempBuy-(tempBuy*0.25);
+                    }
+                }
+            }
+            B.SetBook(idBook,nameBook,author,year,cate,0,0,tempPre);
+            BookList->AddBook(B);
+        }
+        BookFile.close();
+    }
+    else{
+        cout<<"Can not open file"<<endl;
+    }
+}
+
 void BookStoreController::SelectCategoryBook(){
     Book b;
     int menuCate=0;
